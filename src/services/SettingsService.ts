47b5f2537311bc325c9503f57e5c9c5890020f32
Ronald <aandrade.ronald@gmail.com>
2021-04-26 09:40:48 -0300

    comit2_end

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -40,8 +40,12 @@ class SettingsService {
     }
 
     async update( username: string, chat: boolean) {
-        const settings = await this.settingsRepository.createQueryBuilder().update(Setting).set({ chat }).where("username = :username", {
-            username
+        await this.settingsRepository
+        .createQueryBuilder()
+        .update(Setting)
+        .set({ chat })
+        .where("username = :username", {
+         username,
         }).execute();
     }
 }
--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -34,4 +34,11 @@ io.on("connect", async (socket) => {
             socket_id: socket.id,
         });
     });
+
+    socket.on("admin_user_in_support", async params => {
+        const { user_id } = params;
+        await connectionsService.updateAdminID(user_id, socket.id);
+
+        io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+    })
 });
